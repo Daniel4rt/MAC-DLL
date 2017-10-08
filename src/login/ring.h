612d108dfa0ab05a9153517d5a023cc9da3d5e77
Rodrigo Montor Velis <dani3lart@gmail.com>
2017-10-07 20:48:10 -0600

    No-Diff

---
+++ src/login/ring.h
@@ -0,0 +1,7 @@
+//Ring-0	your crc_ring need lengt 32 characters - If it exceeds 32 characters delete the last characters until 32.
+//d7d7da735c6d310f884558f2313aa457		
+//d7da735c6d310f884558f2313aa457
+//d7d7da735c6d310f884558f2313aa457
+#define CRC_RING "38170ad611a6d93683926cbe6c1262" //paste your CPT32 here <<<<<<
+int statusS;
+
